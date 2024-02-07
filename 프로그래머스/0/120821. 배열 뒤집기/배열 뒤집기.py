3b7d7fbc56911972bb6405d05be888b336658f43
bbundnam <145851524+bbundnam@users.noreply.github.com>
2024-02-07 20:56:23 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -0,0 +1,10 @@
+# def solution(num_list):
+#     # num_list.reverse()
+#     answer = num_list.reverse()
+#     return answer
+
+
+def solution(num_list):
+    num_list.reverse()
+    answer = num_list
+    return answer
