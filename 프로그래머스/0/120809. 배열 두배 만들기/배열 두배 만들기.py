16f2292fa53acfbecc10b32ed0ca11b3bee05022
정세홍 <145851524+bbundnam@users.noreply.github.com>
2024-04-30 17:16:37 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -0,0 +1,10 @@
+# def solution(numbers):
+#     answer = []
+#     for i in range(numbers)
+#         i * 2
+#     numbers = answerapend()
+    
+#     return answer
+
+def solution(numbers):
+    return [num * 2 for num in numbers]
