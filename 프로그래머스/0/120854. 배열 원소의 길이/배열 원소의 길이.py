1f545d5425531a689d34ec254d5404114080f22b
정세홍 <145851524+bbundnam@users.noreply.github.com>
2024-04-30 19:51:57 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -0,0 +1,9 @@
+def solution(strlist):
+    answer = []
+    # con = len(strlist)
+    # answer.append(con)
+    # return answer
+    
+    for i in strlist:
+        answer.append(len(i))
+    return answer    
